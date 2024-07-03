5fb8cab3148fd7b2de1bbd6b240c4d21a0172248
Md. Alif Babu <muhammad.alifbabu@gmail.com>
2024-07-03 16:49:03 +0600

    record in spring boot practicing

---
+++ src/main/java/com/example/record/RecordBeginApplication.java
@@ -0,0 +1,13 @@
+package com.example.record;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordBeginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordBeginApplication.class, args);
+	}
+
+}
